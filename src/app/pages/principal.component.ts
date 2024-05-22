693e57039b177179c35e4cb13bafc47f27a7a5ad
Luis <aotourdeveloper1@gmail.com>
2024-05-22 10:41:37 -0500

    Vista responsive de registrar PQR

--- src/app/pages/principal.component.ts
+++ src/app/pages/principal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Component({
   selector: 'app-principal',
@@ -6,15 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.scss'],
 })
 export class PrincipalComponent {
-  eventoTabs: string = 'CONSUL';
+  @Output() navbar: EventEmitter<string> = new EventEmitter<string>();
+  eventoTabs: any = 'CONSUL';
+  window: any = window.innerWidth;
+
+  emitEvent(code: string) {
+    this.navbar.emit(code);
+    this.eventoTabs = code;
+  }
+
   septsSeleccionado: number | string = 0;
 
   actions(event: string) {
     this.eventoTabs = event;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    // Actualiza el valor del ancho del navegador cuando cambia
+    this.window = event.target.innerWidth;
+    console.log('Ancho del navegador:', this.window);
+  }
+
   recibirEvento(event: number) {
-    console.log(event)
+    console.log(event);
     this.septsSeleccionado = event;
   }
 }
